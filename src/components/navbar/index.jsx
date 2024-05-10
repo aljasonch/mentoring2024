0762149c58ca60a9c2eb18003dd58a879978db2f
aljasonch <alfonsusjason01@gmail.com>
2024-05-10 18:10:01 +0700

    edit navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -10,25 +10,25 @@ function Navbar() {
           <img src={Logo} className='w-[6rem] absolute z-10 m-8 ml-[4rem] shadow-object'></img>  
           <img src={Pistol} className='w-[38.5rem] shadow-object lg:w-[30rem] xl:w-[38.5rem]'></img>
         </div>
-        <ul className="flex space-x-[6rem] ml-8 pl-4 pb-16 z-0 lg:space-x-[5rem] 2xl:space-x-[10rem]">
+        <ul className="flex space-x-[6rem] ml-8 pl-4 pb-16 z-0 lg:space-x-[5rem]">
           <div className='w-[9rem] h-[3rem]'>
-            <li className="text-[#2B2476] bg-white w-[7rem] 2xl:w-[9rem] 2xl:h-[3rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500 ">
-              <a href="#" className='text-[0.875rem] 2xl:text-[1.15rem] 2xl:pr-[0.5rem] 2xl:py-[0.6rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>HOME</a>
+            <li className="text-[#2B2476] bg-white w-[7rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500 ">
+              <a href="#" className='text-[0.875rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>HOME</a>
             </li>
           </div>
           <div className="w-[9rem] h-[3rem]">
-            <li className="text-[#2B2476] bg-white w-[7rem] 2xl:w-[9rem] 2xl:h-[3rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
-              <a href="#" className='text-[0.875rem] 2xl:text-[1.15rem] 2xl:pr-[0.5rem] 2xl:py-[0.6rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>FAQ</a>
+            <li className="text-[#2B2476] bg-white w-[7rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
+              <a href="#" className='text-[0.875rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>FAQ</a>
             </li>
           </div>
           <div className="w-[9rem] h-[3rem]">
-            <li className="text-[#2B2476] bg-white w-[7rem] 2xl:w-[9rem] 2xl:h-[3rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
-              <a href="#" className='text-[0.875rem] 2xl:text-[1.15rem] 2xl:pr-[0.5rem] 2xl:py-[0.6rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>ABOUT US</a>
+            <li className="text-[#2B2476] bg-white w-[7rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
+              <a href="#" className='text-[0.875rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>ABOUT US</a>
             </li>
           </div>
           <div className="w-[9rem] h-[3rem]">
-            <li className="text-[#2B2476] bg-white w-[7rem] 2xl:w-[9rem] 2xl:h-[3rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
-              <a href="#" className='text-[0.875rem] 2xl:text-[1.15rem] 2xl:pr-[0.5rem] 2xl:py-[0.6rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>GALLERY</a>
+            <li className="text-[#2B2476] bg-white w-[7rem] h-[2rem] skew-x-[-40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-[1.2] transition-transform duration-500">
+              <a href="#" className='text-[0.875rem] pr-1 py-1 font-medium skew-x-[40deg] inline-block'>GALLERY</a>
             </li>
           </div>
         </ul>
