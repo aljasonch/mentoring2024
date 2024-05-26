d7917d0602be1d175f1ea06d985b2c81b5ab1773
aljasonch <alfonsusjason01@gmail.com>
2024-05-26 11:21:42 +0700

    responsive navbar lg,md,xl

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -7,21 +7,21 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <div className="fixed z-30 w-full xl:h-28 md:h-[60px] background-navbar"></div>
-      <div className="xl:pr-24 container-2xl flex items-center m-auto fixed z-40">
+      <div className="fixed z-30 w-full xl:h-28 md:h-[60px] lg:h-20 background-navbar"></div>
+      <div className="w-full mx-auto flex items-center justify-between fixed z-40">
         <div className="relative">
           <img
             src={Logo}
-            className="xl:w-24 absolute z-10 xl:m-8 md:m-4 md:ml-8 xl:ml-16 shadow-object md:w-12"
+            className="xl:w-24 absolute z-10 xl:m-8 md:m-5 md:ml-8 xl:ml-16 shadow-object md:w-12 lg:w-16 lg:ml-12 lg:m-6"
             alt="Logo"
           />
           <img
             src={Pistol}
-            className="xl:w-[34rem] md:w-[17rem] shadow-object"
+            className="xl:w-[34rem] md:w-72 lg:w-96 shadow-object"
             alt="Pistol"
           />
         </div>
-        <ul className="flex xl:ml-8 xl:pl-4 xl:pb-16 md:pb-6 md:ml-2 z-0 xl:space-x-20 md:space-x-8">
+        <ul className="flex xl:pb-16 mx-auto md:pb-6 lg:pb-10 z-0 xl:gap-x-20 md:gap-x-8 lg:gap-x-12">
           <Link to="/">
             <div className="xl:w-36 xl:h-12 md:w-24 md:h-10">
               <li className="text-[#2B2476] bg-white xl:w-28 xl:h-8 md:w-20 md:h-6 -skew-x-[40deg] shadow-nav rounded-tr-2xl rounded-bl-2xl rounded-tl rounded-br transform hover:scale-110 transition-transform duration-500">
