0eb450d3cea8151384a3de519d421fed1e074719
aljasonch <alfonsusjason01@gmail.com>
2024-08-12 17:36:11 +0700

    test

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -58,28 +58,28 @@ function Countdown({ targetDate, onCountdownComplete }) {
 
   return (
     <div className="flex justify-center xl:gap-24 lg:gap-12 md:gap-6 xss:gap-0 items-center  xss:flex-col md:flex-row">
-      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-3xl">
+      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-4xl">
         {String(timeLeft.days || "0").padStart(2, "0")}
         <span className="xss:text-base md:text-xl block">Days</span>
       </div>
       <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-base">
         <p>:</p>
       </div>
-      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-3xl">
+      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-4xl">
         {String(timeLeft.hours || "0").padStart(2, "0")}
         <span className="xss:text-base md:text-xl block">Hours</span>
       </div>
       <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-base">
         <p>:</p>
       </div>
-      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-3xl">
+      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-4xl">
         {String(timeLeft.minutes || "0").padStart(2, "0")}
         <span className="xss:text-base md:text-xl block">Minutes</span>
       </div>
       <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-base">
         <p>:</p>
       </div>
-      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-3xl">
+      <div className="text-center xl:text-8xl lg:text-6xl md:text-5xl xss:text-4xl">
         {String(timeLeft.seconds || "0").padStart(2, "0")}
         <span className="xss:text-base md:text-xl block">Seconds</span>
       </div>
