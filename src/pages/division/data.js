5e74232b96d9c1525a780c605b7bab107ad8720d
aljasonch <alfonsusjason01@gmail.com>
2024-09-02 22:30:05 +0700

    update foto division

--- src/pages/division/data.js
+++ src/pages/division/data.js
@@ -1,115 +1,153 @@
+import Accom from "../../assets/Accom_Icon.webp";
+import Admin from "../../assets/Admin_Icon.webp";
+import CC from "../../assets/CC_Icon.webp";
+import Dokum from "../../assets/Dokum_Icon.webp";
+import Exc from "../../assets/Exc_Icon.webp";
+import Insurer from "../../assets/Insurer_Icon.webp";
+import Mentor from "../../assets/Mentor_Icon.webp";
+import Medpar from "../../assets/Medpar_Icon.webp";
+import Research from "../../assets/Research_Icon.webp";
+import Visual from "../../assets/Visual_Icon.webp";
+import Website from "../../assets/Website_Icon.webp";
+import Accom_Picture from "../../assets/divisi/Accom.avif";
+import Mentor_1 from "../../assets/divisi/Mentor.avif";
+import Insurer_Picture from "../../assets/divisi/Insurer.avif";
+import Medpar_Picture from "../../assets/divisi/Medpar.avif";
+import Website_Picture from "../../assets/divisi/Website.avif";
+import Exc_Picture from "../../assets/divisi/EXC.avif";
+import Research_Picture from "../../assets/divisi/Research.avif";
+import Visual_Picture from "../../assets/divisi/Visual.avif";
+import CC_Picture from "../../assets/divisi/CC.avif";
+import Dokum_Picture from "../../assets/divisi/Docum.avif";
+import Admin_Picture from "../../assets/divisi/Admin.avif";
+import Mentor_2 from "../../assets/divisi/Mentor_2.avif";
+import Mentor_3 from "../../assets/divisi/Mentor_3.avif";
+import Mentor_4 from "../../assets/divisi/Mentor_4.avif";
+import Mentor_5 from "../../assets/divisi/Mentor_5.avif";
+import Mentor_6 from "../../assets/divisi/Mentor_6.avif";
+import Mentor_7 from "../../assets/divisi/Mentor_7.avif";
+import Mentor_8 from "../../assets/divisi/Mentor_8.avif";
+import Mentor_9 from "../../assets/divisi/Mentor_9.avif";
+import Mentor_10 from "../../assets/divisi/Mentor_10.avif";
+import Mentor_11 from "../../assets/divisi/Mentor_11.avif";
+import Mentor_12 from "../../assets/divisi/Mentor_12.avif";
+import Mentor_13 from "../../assets/divisi/Mentor_13.avif";
+import Mentor_14 from "../../assets/divisi/Mentor_14.avif";
 
-import Accom from '../../assets/Accom_Icon.webp';
-import Admin from '../../assets/Admin_Icon.webp';
-import CC from '../../assets/CC_Icon.webp';
-import Dokum from '../../assets/Dokum_Icon.webp';
-import Exc from '../../assets/Exc_Icon.webp';
-import Insurer from '../../assets/Insurer_Icon.webp';
-import Mentor from '../../assets/Mentor_Icon.webp';
-import Medpar from '../../assets/Medpar_Icon.webp';
-import Research from '../../assets/Research_Icon.webp';
-import Visual from '../../assets/Visual_Icon.webp';
-import Website from '../../assets/Website_Icon.webp';
-import Accom_Picture from "../../assets/divisi.webp";
-import Mentor_Picture from "../../assets/divisi.webp";
-import Insurer_Picture from "../../assets/divisi.webp";
-import Medpar_Picture from "../../assets/divisi.webp";
-import Website_Picture from "../../assets/divisi.webp";
-import Exc_Picture from "../../assets/divisi.webp";
-import Research_Picture from "../../assets/divisi.webp";
-import Visual_Picture from "../../assets/divisi.webp";
-import CC_Picture from "../../assets/divisi.webp";
-import Dokum_Picture from "../../assets/divisi.webp";
-import Admin_Picture from "../../assets/divisi.webp";
 const data = [
-    {
-        id: 1,
-        title: 'Accomodation',
-        description: 'Menyediakan sarana dan prasarana yang dibutuhkan selama kegiatan Mentoring 2024 berlangsung.',
-        image: Accom,
-        hoverColor: '#9CF9FF',
-        picture: Accom_Picture,
-    },
-    {
-        id: 2,
-        title: 'Administration',
-        description: 'Melakukan pendataan dan mengatur kebutuhan administratif yang dibutuhkan dalam Mentoring 2024.',
-        image: Admin,
-        hoverColor: '#E1ED7B',
-        picture: Admin_Picture,
-    },
-    {
-        id: 3,
-        title: 'Content Creator',
-        description: 'Menyusun ide, konsep, dan materi acara Mentoring 2024, mempersiapkan modul materi untuk mentee, serta aktif mengkoordinasikan pelatihan mentor.',
-        image: CC,
-        hoverColor: '#FFFBB9',
-        picture: CC_Picture,
-    },
-    {
-        id: 4,
-        title: 'Documentation',
-        description: 'Mendokumentasikan segala kegiatan dalam Mentoring 2024 dalam bentuk foto dan video.',
-        image: Dokum,
-        hoverColor: '#DCB5FF',
-        picture: Dokum_Picture,
-    },
-    {
-        id: 5,
-        title: 'Executive Committe',
-        description: 'Bertanggung jawab dalam membangun kerjasama yang efektif dan dinamis di antara seluruh divisi Mentoring UMN.',
-        image: Exc,
-        hoverColor: '#94C3FF',
-        picture: Exc_Picture,
-    },
-    {
-        id: 6,
-        title: 'Insurer',
-        description: 'Menjaga keamanan dan kekondusifan serta membentuk peraturan yang ada di Mentoring 2024.',
-        image: Insurer,
-        hoverColor: '#CCCCCC',
-        picture: Insurer_Picture,
-    },
-    {
-        id: 7,
-        title: 'Mentor',
-        description: 'Membina mentee dalam mengikuti seluruh kegiatan Mentoring 2024.',
-        image: Mentor,
-        hoverColor: '#FFC0A5',
-        picture: Mentor_Picture,
-    },
-    {
-        id: 8,
-        title: 'PR & Medpar',
-        description: 'Merancang konten dan mengelola publikasi untuk kegiatan Mentoring 2024, serta menjalin kerjasama dengan media partner untuk mendukung visibilitas acara.',
-        image: Medpar,
-        hoverColor: '#FFC0C0',
-        picture: Medpar_Picture,
-    },
-    {
-        id: 9,
-        title: 'Research',
-        description: 'Merancang metode evaluasi untuk Mentoring 2024.',
-        image: Research,
-        hoverColor: '#FFD4A9',
-        picture: Research_Picture,
-    },
-    {
-        id: 10,
-        title: 'Visual',
-        description: 'Membuat visualisasi desain yang akan digunakan selama Mentoring 2024.',
-        image: Visual,
-        hoverColor: '#FFCEE5',
-        picture: Visual_Picture,
-    },
-    {
-        id: 11,
-        title: 'Website',
-        description: 'Membuat, mengelola, dan memperbarui kinerja website Mentoring 2024 selama rangkaian kegiatan Mentoring 2024 berlangsung.',
-        image: Website,
-        hoverColor: '#BBE0FF',
-        picture: Website_Picture,
-    }
-]
+  {
+    id: 1,
+    title: "Accomodation",
+    description:
+      "Menyediakan sarana dan prasarana yang dibutuhkan selama kegiatan Mentoring 2024 berlangsung.",
+    image: Accom,
+    hoverColor: "#9CF9FF",
+    picture: [Accom_Picture],
+  },
+  {
+    id: 2,
+    title: "Administration",
+    description:
+      "Melakukan pendataan dan mengatur kebutuhan administratif yang dibutuhkan dalam Mentoring 2024.",
+    image: Admin,
+    hoverColor: "#E1ED7B",
+    picture: [Admin_Picture],
+  },
+  {
+    id: 3,
+    title: "Content Creator",
+    description:
+      "Menyusun ide, konsep, dan materi acara Mentoring 2024, mempersiapkan modul materi untuk mentee, serta aktif mengkoordinasikan pelatihan mentor.",
+    image: CC,
+    hoverColor: "#FFFBB9",
+    picture: [CC_Picture],
+  },
+  {
+    id: 4,
+    title: "Documentation",
+    description:
+      "Mendokumentasikan segala kegiatan dalam Mentoring 2024 dalam bentuk foto dan video.",
+    image: Dokum,
+    hoverColor: "#DCB5FF",
+    picture: [Dokum_Picture],
+  },
+  {
+    id: 5,
+    title: "Executive Committe",
+    description:
+      "Bertanggung jawab dalam membangun kerjasama yang efektif dan dinamis di antara seluruh divisi Mentoring UMN.",
+    image: Exc,
+    hoverColor: "#94C3FF",
+    picture: [Exc_Picture],
+  },
+  {
+    id: 6,
+    title: "Insurer",
+    description:
+      "Menjaga keamanan dan kekondusifan serta membentuk peraturan yang ada di Mentoring 2024.",
+    image: Insurer,
+    hoverColor: "#CCCCCC",
+    picture: [Insurer_Picture],
+  },
+  {
+    id: 7,
+    title: "Mentor",
+    description:
+      "Membina mentee dalam mengikuti seluruh kegiatan Mentoring 2024.",
+    image: Mentor,
+    hoverColor: "#FFC0A5",
+    picture: [
+      Mentor_1,
+      Mentor_2,
+      Mentor_3,
+      Mentor_4,
+      Mentor_5,
+      Mentor_6,
+      Mentor_7,
+      Mentor_8,
+      Mentor_9,
+      Mentor_10,
+      Mentor_11,
+      Mentor_12,
+      Mentor_13,
+      Mentor_14,
+    ],
+  },
+  {
+    id: 8,
+    title: "PR & Medpar",
+    description:
+      "Merancang konten dan mengelola publikasi untuk kegiatan Mentoring 2024, serta menjalin kerjasama dengan media partner untuk mendukung visibilitas acara.",
+    image: Medpar,
+    hoverColor: "#FFC0C0",
+    picture: [Medpar_Picture],
+  },
+  {
+    id: 9,
+    title: "Research",
+    description: "Merancang metode evaluasi untuk Mentoring 2024.",
+    image: Research,
+    hoverColor: "#FFD4A9",
+    picture: [Research_Picture],
+  },
+  {
+    id: 10,
+    title: "Visual",
+    description:
+      "Membuat visualisasi desain yang akan digunakan selama Mentoring 2024.",
+    image: Visual,
+    hoverColor: "#FFCEE5",
+    picture: [Visual_Picture],
+  },
+  {
+    id: 11,
+    title: "Website",
+    description:
+      "Membuat, mengelola, dan memperbarui kinerja website Mentoring 2024 selama rangkaian kegiatan Mentoring 2024 berlangsung.",
+    image: Website,
+    hoverColor: "#BBE0FF",
+    picture: [Website_Picture],
+  },
+];
 
 export default data;
--- src/pages/division/index.jsx
+++ src/pages/division/index.jsx
@@ -1,21 +1,54 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import "./style.css";
 import Daco from "../../assets/Blackdots_3.webp";
-import data from "../division/data.js";
 import Screen from "../../assets/screen.webp";
 import ScreenMobile from "../../assets/screenMobile.webp";
 import Button from "../../assets/button.webp";
+import data from "../division/data.js";
+import Mentor_2 from "../../assets/divisi/Mentor_2.avif";
+import Mentor_3 from "../../assets/divisi/Mentor_3.avif";
+import Mentor_4 from "../../assets/divisi/Mentor_4.avif";
+import Mentor_5 from "../../assets/divisi/Mentor_5.avif";
+import Mentor_6 from "../../assets/divisi/Mentor_6.avif";
+import Mentor_7 from "../../assets/divisi/Mentor_7.avif";
+import Mentor_8 from "../../assets/divisi/Mentor_8.avif";
+import Mentor_9 from "../../assets/divisi/Mentor_9.avif";
+import Mentor_10 from "../../assets/divisi/Mentor_10.avif";
+import Mentor_11 from "../../assets/divisi/Mentor_11.avif";
+import Mentor_12 from "../../assets/divisi/Mentor_12.avif";
+import Mentor_13 from "../../assets/divisi/Mentor_13.avif";
+import Mentor_14 from "../../assets/divisi/Mentor_14.avif";
 
-// Preload critical images
-const preloadImages = [Daco, Screen, ScreenMobile, Button];
+// Preload critical images and mentor images
+const preloadImages = [
+  Daco,
+  Screen,
+  ScreenMobile,
+  Button,
+  Mentor_2,
+  Mentor_3,
+  Mentor_4,
+  Mentor_5,
+  Mentor_6,
+  Mentor_7,
+  Mentor_8,
+  Mentor_9,
+  Mentor_10,
+  Mentor_11,
+  Mentor_12,
+  Mentor_13,
+  Mentor_14,
+];
 
 function Division() {
   const [selectedData, setSelectedData] = useState(null);
   const [imagesLoaded, setImagesLoaded] = useState(false);
   const [isCardVisible, setIsCardVisible] = useState(false);
-  const [animationClass, setAnimationClass] = useState('');
+  const [animationClass, setAnimationClass] = useState("");
 
   useEffect(() => {
     const loadImage = (src) => {
@@ -27,9 +60,9 @@ function Division() {
       });
     };
 
-    Promise.all(preloadImages.map(src => loadImage(src)))
+    Promise.all(preloadImages.map((src) => loadImage(src)))
       .then(() => setImagesLoaded(true))
-      .catch(err => console.error("Failed to preload images", err));
+      .catch((err) => console.error("Failed to preload images", err));
   }, []);
 
   const handleCardClick = useCallback((item) => {
@@ -42,13 +75,24 @@ function Division() {
     setIsCardVisible(true);
   }, []);
 
+  const handleImageClick = useCallback(() => {
+    if (selectedData && Array.isArray(selectedData.picture)) {
+      Fancybox.show(
+        selectedData.picture.map((src) => ({ src })),
+        {
+          // Customize Fancybox options here
+        }
+      );
+    }
+  }, [selectedData]);
+
   const handleCloseCard = useCallback(() => {
-    setAnimationClass('slide-down');
+    setAnimationClass("slide-down");
     setTimeout(() => {
       setIsCardVisible(false);
       setSelectedData(null);
-      setAnimationClass('');
-    }, 500); // Match this duration with the CSS animation duration
+      setAnimationClass("");
+    }, 500); // Ensure this matches the CSS animation duration
   }, []);
 
   if (!imagesLoaded) {
@@ -105,7 +149,11 @@ function Division() {
         <img src={Daco} alt="Daco" />
       </div>
       {selectedData && (
-        <div className={`xss:w-[53.125rem] overflow-hidden card-container ${animationClass} xss:top-[50%] sm:top-1/2 lg:top-[55%] transition-opacity duration-300 ${isCardVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <div
+          className={`xss:w-[53.125rem] overflow-hidden card-container ${animationClass} xss:top-[50%] sm:top-1/2 lg:top-[55%] transition-opacity duration-300 ${
+            isCardVisible ? "opacity-100" : "opacity-0"
+          }`}
+        >
           <div className="relative" id="screenPopUp">
             <div className="absolute bg-[#02B4A8] border-2 border-[rgb(24,230,177)] md:rounded-tl-[15px] md:rounded-br-[15px] flex items-center justify-center lg:w-64 lg:h-14 lg:top-[22%] lg:right-[7%] sm:w-60 sm:h-14 md:w-40 md:h-[34px] md:top-[22%] md:right-[10%] xss:w-32 xss:h-8 xss:top-[50%] xss:rounded-tl-[10px] xss:rounded-br-[10px] xs:w-40 xs:h-10 xs:top-[50%] center">
               <p className="text-[#F5FDFF] text-center lg:text-[18px] md:text-[12px] xss:text-[10px] spyagencyRegular sm:text-base">
@@ -113,12 +161,21 @@ function Division() {
               </p>
             </div>
             <div className="absolute md:w-[48%] xss:w-[87%] center md:top-[20%] lg:top-[20%] xss:top-[12%] md:left-[7%]">
-              <LazyLoadImage
-                className="w-full xss:mt-2 md:pb-4 "
-                src={selectedData.picture}
-                alt={selectedData.title}
-                effect="blur"
-              />
+              <div className="relative group">
+                <LazyLoadImage
+                  src={selectedData.picture[0]} // Display the first image or a thumbnail in the pop-up
+                  alt={selectedData.title}
+                  effect="blur"
+                  className="object-cover transition duration-300 ease-in-out group-hover:grayscale"
+                  onClick={handleImageClick}
+                  style={{ cursor: "pointer" }}
+                />
+                <div className="absolute inset-0 flex items-center justify-center pointer-events-none text-white opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                  <p className="text-lg bg-opacity-50 p-2 rounded">
+                    Click to view
+                  </p>
+                </div>
+              </div>
             </div>
             <button
               className="xss:w-[72px] md:w-[120px] fixed xss:right-[9%] md:right-[7%] xss:top-[3%] md:top-[0%]"
@@ -130,13 +187,16 @@ function Division() {
               {selectedData.description}
             </p>
             <img src={Screen} className="hidden md:block" alt="Screen" />
-            <img src={ScreenMobile} className="md:hidden block" alt="Mobile Screen" />
+            <img
+              src={ScreenMobile}
+              className="md:hidden block"
+              alt="Mobile Screen"
+            />
             <div className="bg-white flex justify-center card-container absolute md:top-[75%] xss:top-[82%] xs:top-[81%] xss:left-[50%] xss:max-w-12 xs:max-w-14 sm:max-w-20 sm:p-4 md:max-w-[80%] md:left-[84%] rounded-full shadow-lg xss:p-2 md:p-5">
               <LazyLoadImage
                 src={selectedData.image}
-                className="object-cover"
+                className="object-cover transition duration-300 ease-in-out group-hover:grayscale"
                 alt={selectedData.title}
-                effect="blur"
               />
             </div>
           </div>
