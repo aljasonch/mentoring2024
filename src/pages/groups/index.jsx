39282d1c5374d953d17710d63b1360129bec2f1a
christianyoga13 <37493232+christianyoga13@users.noreply.github.com>
2024-09-02 13:41:37 +0700

    Up Buttton back

--- src/pages/groups/index.jsx
+++ src/pages/groups/index.jsx
@@ -219,22 +219,22 @@ export default function Groups() {
   }, [state, setState]);
 
   useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 300) {
-          setShowButton(true);
-        } else {
-          setShowButton(false);
-        }
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
-  
-    // Function to scroll back to the top
-    const scrollToTop = () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
     };
+  
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
+    
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   
   return (
     <div>
