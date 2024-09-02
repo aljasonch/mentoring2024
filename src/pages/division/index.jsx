628baa78451cd1e08f34e4520ed14f10e88c7029
aljasonch <alfonsusjason01@gmail.com>
2024-09-02 23:02:15 +0700

    give instruction page division on mobile

--- src/pages/division/index.jsx
+++ src/pages/division/index.jsx
@@ -180,8 +180,8 @@ function Division() {
                   Tap to view
                 </div>
                 {/* Hover text for desktop */}
-                <div className="sm:block hidden absolute inset-0 flex items-center justify-center pointer-events-none text-white opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
-                  <p className="text-lg bg-opacity-50 p-2 rounded">
+                <div className="absolute inset-0 flex items-center justify-center pointer-events-none text-white opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                  <p className="text-lg bg-opacity-50 p-2 rounded sm:block hidden">
                     Click to view
                   </p>
                 </div>
